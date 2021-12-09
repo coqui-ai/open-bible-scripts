97beff8c1b58b628639a3df86fe5d90d93b2bbad
Josh Meyer <josh@coqui.ai>
2021-12-09 22:35:02 +0000

    add print statement and wait

--- parallelize.sh
+++ parallelize.sh
@@ -10,3 +10,6 @@ cd data
 for i in ../*/*.txt; do
     ( DIR=$(echo $i|cut -d'/' -f3|cut -d'.' -f1); mkdir $DIR; cp $i -t $DIR; cp ${i/.txt/}.wav -t $DIR ) &
 done
+wait
+
+echo "I: Done setting up parallelization for MFA"

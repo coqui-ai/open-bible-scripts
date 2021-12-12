1b411b077dac21102adae9ac96dddbf23c318c99
Josh Meyer <josh@coqui.ai>
2021-12-12 02:44:03 +0000

    cleaner parallelizing

--- parallelize.sh
+++ parallelize.sh
@@ -3,36 +3,27 @@
 # must run clean.sh first
 # should be run from within directory where biblica data is downloaded and already unzipped
 
-cut-clean() {
-    i="$1"
-    AUTHOR=$(echo $i | cut -d'_' -f3)
-    PART=$(echo $i | cut -d'_' -f4)
-    if [[ "${#PART}" -eq 2 ]]; then
-	PART="0$PART"
-    fi
-    cat $i | cut -f3 > $AUTHOR/${AUTHOR}_${PART}.txt
-}
-
-export -f cut-clean
-
-parallel --eta cut-clean ::: *.clean
-
 mkdir data
-cd data
 
-process-for-mfa() {
-    i="$1"
-    DIR=$(echo $i|cut -d'/' -f3|cut -d'.' -f1);
-    mkdir $DIR;
-    mv $i -t $DIR
-    mv ${i/.txt/}.wav -t $DIR
+parallelize-text() {
+    i="$1";
+    AUTHOR=$(echo $i | cut -d'_' -f3);
+    PART=$(echo $i | cut -d'_' -f4 | sed -E 's/^0+//g');
+    mkdir -p data/${AUTHOR}_${PART};
+    cat $i | cut -f3 > data/${AUTHOR}_${PART}/${AUTHOR}_${PART}.txt;
 }
 
-export -f process-for-mfa
+parallelize-audio() {
+    i="$1";
+    AUTHOR=$(echo $i | cut -d'/' -f2 | cut -d'_' -f1);
+    PART=$(echo $i | cut -d'/' -f2 | cut -d'_' -f2 | sed 's/.wav//g' | sed -E 's/^0+//g');
+    mv $i data/${AUTHOR}_${PART}/${AUTHOR}_${PART}.wav;
+}
 
-parallel --eta process-for-mfa ::: ../*/*.txt
+export -f parallelize-text
+export -f parallelize-audio
+
+parallel --eta parallelize-text ::: *.clean
+parallel --eta parallelize-audio ::: */*.wav
 
 echo "I: Done setting up parallelization for MFA"
-#for i in ../*/*.txt; do
-#    ( DIR=$(echo $i|cut -d'/' -f3|cut -d'.' -f1); mkdir $DIR; cp $i -t $DIR; cp ${i/.txt/}.wav -t $DIR ) &
-#done

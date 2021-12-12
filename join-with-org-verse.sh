77045ba40b9ae8f6e11cf868da5adfa213399243
Josh Meyer <josh@coqui.ai>
2021-12-12 18:20:01 +0000

    finalize join script

--- join-with-org-verse.sh
+++ join-with-org-verse.sh
@@ -1,6 +1,9 @@
-CHAPTER=$1
+#!/bin/bash
+
+BEST_VERSES=$1
+CHAPTER=$(echo $BEST_VERSES | cut -d'/' -f3)
 
 join -t'	' -j3 \
-     <( sort -t '	' -k3 org/*${CHAPTER}*clean*) \
-     <( sort -t '	' -k3 all/$CHAPTER/*best*) \
-     > all/$CHAPTER/joined.tsv
+     <( sort -t '	' -k3 *${CHAPTER}*.clean) \
+     <( sort -t '	' -k3 $BEST_VERSES) \
+     > data/mfa-output/$CHAPTER/joined.tsv

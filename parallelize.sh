b2d7ab2e601df982b5c9528d0e475eb31c9e22b3
Josh Meyer <josh@coqui.ai>
2021-12-13 22:23:22 +0000

    better leading zeros removal

--- parallelize.sh
+++ parallelize.sh
@@ -6,17 +6,17 @@
 mkdir data
 
 parallelize-text() {
-    i="$1";
+    i=$( echo $1 | sed -E 's/^0+//g');
     AUTHOR=$(echo $i | cut -d'_' -f3);
-    PART=$(echo $i | cut -d'_' -f4 | sed -E 's/^0+//g');
+    PART=$(echo $i | cut -d'_' -f4);
     mkdir -p data/${AUTHOR}_${PART};
     cat $i | cut -f3 > data/${AUTHOR}_${PART}/${AUTHOR}_${PART}.txt;
 }
 
 parallelize-audio() {
-    i="$1";
+    i=$( echo $1 | sed -E 's/^0+//g');
     AUTHOR=$(echo $i | cut -d'/' -f2 | cut -d'_' -f1);
-    PART=$(echo $i | cut -d'/' -f2 | cut -d'_' -f2 | sed 's/.wav//g' | sed -E 's/^0+//g');
+    PART=$(echo $i | cut -d'/' -f2 | cut -d'_' -f2 | sed 's/.wav//g');
     mv $i data/${AUTHOR}_${PART}/${AUTHOR}_${PART}.wav;
 }
 
